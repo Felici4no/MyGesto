3786e4ff99393fdb058b2d577c7da5e48dd795ed
Felici4no <lucas.for.study.42@gmail.com>
2025-12-28 22:13:16 -0300

    feat: add example environment variables and configure Next.js server actions for local network access.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Allow mobile testing on local network
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', '192.168.15.6:3000', '192.168.*.*:3000']
+    }
+  }
 };
 
 export default nextConfig;
